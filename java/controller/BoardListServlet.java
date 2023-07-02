d3ea9660f42ef5b3824fe2d13dd3af786be967f2
dlehdduq22 <dlehdduq22@naver.com>
2023-07-02 23:22:29 +0900

    게시판 전체조회 클래스

---
+++ java/controller/BoardListServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BoardDao;
+import vo.Board;
+
+@WebServlet("/boardList")
+public class BoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardDao bDao = new BoardDao();
+		
+		ArrayList<Board> list = bDao.getBoardList();
+		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("boardList.jsp").forward(request, response);
+	}
+}
